2736369a791c098599f0405be474e33071788f85
sergejgluhov <arane1223@gmail.com>
2025-11-06 10:30:05 +0500

    added test

--- src/test/java/tests/WikiMobileTests.java
+++ src/test/java/tests/WikiMobileTests.java
@@ -1,10 +1,14 @@
 package tests;
 
+import com.codeborne.selenide.SelenideElement;
+import io.appium.java_client.AppiumBy;
 import io.qameta.allure.Owner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 import static io.appium.java_client.AppiumBy.*;
 import static io.qameta.allure.Allure.step;
@@ -16,11 +20,30 @@ public class WikiMobileTests extends TestBase {
     @Test
     @DisplayName("Успешное открытие статьи")
     void successfulArticleOpeningTest() {
-        $(accessibilityId("Search Wikipedia")).click();
-        $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("board games");
+        // Явное ожидание появления кнопки поиска
+        $(AppiumBy.accessibilityId("Search Wikipedia"))
+                .shouldBe(visible)
+                .click();
 
+        // Ожидание ввода текста поиска
+        $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text"))
+                .shouldBe(visible)
+                .sendKeys("board game");
 
+        // Ожидание появления результата поиска и проверка названия
+        SelenideElement result = $(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
+                .shouldHave(text("Board game"))
+                .shouldBe(visible);
+
+        result.click();
+
+        // Проверка, что открылась статья с нужным заголовком
+        $(AppiumBy.id("org.wikipedia.alpha:id/view_page_title_text"))
+                .shouldHave(text("Board game"))
+                .shouldBe(visible);
     }
+
+
 
     @Test
     @DisplayName("Успешный поиск по слову «Appium»")
